bf4cac12022cb559e3501bd74ed9d0314954e489
X00004182 <wangyidong@huaqin.com>
2021-08-04 15:35:15 +0800

    加入显示天气信息功能。

--- app/src/main/java/com/wydweather/android/util/Utility.java
+++ app/src/main/java/com/wydweather/android/util/Utility.java
@@ -2,9 +2,11 @@ package com.wydweather.android.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.wydweather.android.db.City;
 import com.wydweather.android.db.County;
 import com.wydweather.android.db.Provice;
+import com.wydweather.android.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -77,5 +79,19 @@ public class Utility {
             }
         }
         return false;
+    }
+
+
+    //JSON
+    public  static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return  new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
