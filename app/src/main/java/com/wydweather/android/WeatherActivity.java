40e747026d0cf1d38856382c5af6687827ff122a
X00004182 <wangyidong@huaqin.com>
2021-08-04 16:28:26 +0800

    增加后台自动更新天气的功能。

--- app/src/main/java/com/wydweather/android/WeatherActivity.java
+++ app/src/main/java/com/wydweather/android/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.wydweather.android.gson.Forecast;
 import com.wydweather.android.gson.Weather;
+import com.wydweather.android.service.AutoUpdateService;
 import com.wydweather.android.util.HttpUtil;
 import com.wydweather.android.util.Utility;
 
@@ -216,5 +218,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent=new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
